f6572ee57303651758db0d50656508f8c3fded78
Jacob Leeming <jacobtophatleeming@gmail.com>
2020-10-21 12:36:20 +0100

    further refined build process.

--- compileAndRun.bash
+++ compileAndRun.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # compile
-javac -d bin -cp bin src/com/jpl/fyp/classLibrary/TokenType.java
-javac -d bin -cp bin src/com/jpl/fyp/classLibrary/Token.java
-javac -d bin -cp bin src/com/jpl/fyp/compilerComponent/Lexer.java
-javac -d bin -cp bin src/com/jpl/fyp/Main.java
+javac -d bin -cp bin $(find . -name "*.java")
+
+# javac -d bin -cp bin src/com/jpl/fyp/classLibrary/TokenType.java
+# javac -d bin -cp bin src/com/jpl/fyp/classLibrary/Token.java
+# javac -d bin -cp bin src/com/jpl/fyp/compilerComponent/Lexer.java
+# javac -d bin -cp bin src/com/jpl/fyp/Main.java
 
 # run
-java -cp bin com.jpl.fyp.Main
+java -cp bin com.jpl.fyp.Main $1
