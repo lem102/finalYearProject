b85f435f4cf2a1e2481a53d4751fe015e4e9bfbe
Jacob Leeming <jacobtophatleeming@gmail.com>
2020-10-21 13:01:53 +0100

    removed unnecessary comments

--- compileAndRun.bash
+++ compileAndRun.bash
@@ -3,10 +3,5 @@
 # compile
 javac -d bin -cp bin $(find . -name "*.java")
 
-# javac -d bin -cp bin src/com/jpl/fyp/classLibrary/TokenType.java
-# javac -d bin -cp bin src/com/jpl/fyp/classLibrary/Token.java
-# javac -d bin -cp bin src/com/jpl/fyp/compilerComponent/Lexer.java
-# javac -d bin -cp bin src/com/jpl/fyp/Main.java
-
 # run
 java -cp bin com.jpl.fyp.Main $1
