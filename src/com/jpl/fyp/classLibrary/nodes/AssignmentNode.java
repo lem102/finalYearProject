88686563870ed09bea3798fb2edae0267a0b9635
Jacob Leeming <jacobtophatleeming@gmail.com>
2020-11-22 17:18:05 +0000

    improved code of assignmentNode

--- src/com/jpl/fyp/classLibrary/nodes/AssignmentNode.java
+++ src/com/jpl/fyp/classLibrary/nodes/AssignmentNode.java
@@ -1,12 +1,46 @@
 package com.jpl.fyp.classLibrary.nodes;
 
+import java.util.Arrays;
+
+import com.jpl.fyp.classLibrary.JPLException;
+import com.jpl.fyp.classLibrary.Token;
+import com.jpl.fyp.classLibrary.TokenType;
+import com.jpl.fyp.compilerComponent.Parser;
+
 public class AssignmentNode extends StatementNode
 {
     public String assignmentTarget;
 
     public ExpressionNode expression;
 
-    @Override
+    public AssignmentNode(Token[] tokens,
+                          RootNode rootNode) throws JPLException
+    {
+        validateTokens(tokens, rootNode);
+        this.assignmentTarget = tokens[0].tokenValue;
+        // this will need changing when expression node is refactored.
+        this.expression = new ExpressionNode();
+        this.expression.expressionTokens = Arrays.asList(Arrays.copyOfRange(tokens,
+                                                                            2,
+                                                                            tokens.length));
+    }
+
+    private void validateTokens(Token[] tokens,
+                                RootNode rootNode) throws JPLException
+    {
+        if (tokens[0].tokenType != TokenType.Identifier)
+        {
+            Parser.throwParserException(rootNode,
+                                        "Assignment Node : first token is not an identifier token.");
+        }
+        else if (tokens[1].tokenType != TokenType.Assignment)
+        {
+            Parser.throwParserException(rootNode,
+                                        "Assignment Node : second token is not an assignment token.");
+        }
+	}
+
+	@Override
     public String toString()
     {
         var output = "Assignment Node:\n";
--- src/com/jpl/fyp/classLibrary/nodes/WhileNode.java
+++ src/com/jpl/fyp/classLibrary/nodes/WhileNode.java
@@ -1,17 +1,48 @@
 package com.jpl.fyp.classLibrary.nodes;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.jpl.fyp.classLibrary.JPLException;
+import com.jpl.fyp.classLibrary.Token;
+import com.jpl.fyp.classLibrary.TokenType;
+import com.jpl.fyp.compilerComponent.Parser;
 
 public class WhileNode extends ContainingNode
 {
     public ExpressionNode testExpression;
 
-    public WhileNode()
+    public WhileNode(Token[] tokens, RootNode rootNode) throws JPLException
     {
-        statements = new ArrayList<StatementNode>();
+        validateTokens(tokens, rootNode);
+        Token[] expressionTokens = Arrays.copyOfRange(tokens, 2, tokens.length - 1);
+        // needs to be changed when expressionNode is refactored :)
+        this.testExpression = new ExpressionNode();
+        testExpression.expressionTokens = Arrays.asList(expressionTokens);
+        this.statements = new ArrayList<StatementNode>();
     }
 
-    @Override
+	private void validateTokens(Token[] tokens,
+                                RootNode rootNode) throws JPLException
+    {
+        if (tokens[0].tokenType != TokenType.While)
+        {
+            Parser.throwParserException(rootNode,
+                                        "While Node : first token is not while keyword.");
+        }
+        else if (tokens[1].tokenType != TokenType.OpeningParenthesis)
+        {
+            Parser.throwParserException(rootNode,
+                                        "While Node : second token is not an opening parenthesis.");
+        }
+        else if (tokens[tokens.length - 1].tokenType != TokenType.ClosingParenthesis)
+        {
+            Parser.throwParserException(rootNode,
+                                        "While Node : second to last token is not a closing parenthesis.");
+        }
+	}
+
+	@Override
     public String toString()
     {
         String output = "";
