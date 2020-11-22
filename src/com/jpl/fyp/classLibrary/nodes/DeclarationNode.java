3555fbfeaddbb02a6edf656527e3cb29c4e3d259
Jacob Leeming <jacobtophatleeming@gmail.com>
2020-11-22 23:08:29 +0000

    refactored declaration node

--- src/com/jpl/fyp/classLibrary/nodes/DeclarationNode.java
+++ src/com/jpl/fyp/classLibrary/nodes/DeclarationNode.java
@@ -1,6 +1,12 @@
 package com.jpl.fyp.classLibrary.nodes;
 
+import java.util.Arrays;
+
+import com.jpl.fyp.classLibrary.JPLException;
 import com.jpl.fyp.classLibrary.JPLType;
+import com.jpl.fyp.classLibrary.Token;
+import com.jpl.fyp.classLibrary.TokenType;
+import com.jpl.fyp.compilerComponent.Parser;
 
 public class DeclarationNode extends StatementNode
 {
@@ -10,7 +16,62 @@ public class DeclarationNode extends StatementNode
 
     public ExpressionNode expression;
 
-    @Override
+    public DeclarationNode(Token[] tokens, RootNode rootNode)
+        throws JPLException
+    {
+        this.validateTokens(tokens, rootNode);
+        this.type = this.tokenToType(tokens[0]);
+        this.identifier = tokens[1].tokenValue;
+
+        // TODO: needs to be changed when expression node is refactored
+        if (tokens.length > 2)
+        {
+            this.expression = new ExpressionNode();
+            Token[] expressionTokens = Arrays.copyOfRange(tokens, tokens.length-1, tokens.length);
+            expression.expressionTokens = Arrays.asList(expressionTokens);
+        }
+	}
+
+	private JPLType tokenToType(Token token) throws JPLException
+    {
+        if (token.tokenType == TokenType.IntegerDeclaration)
+        {
+            return JPLType.Integer;
+        }
+        else
+        {
+            throw new JPLException("Declaration Node : Invalid type.");
+        }
+	}
+
+	private void validateTokens(Token[] tokens, RootNode rootNode)
+        throws JPLException
+    {
+        if (tokens[0].tokenType != TokenType.IntegerDeclaration)
+        {
+            Parser.throwParserException(rootNode,
+                                        "Declaration Node : First token must be a declaration token. Currently there are only integer declarations available.");
+        }
+        else if (tokens[1].tokenType != TokenType.Identifier)
+        {
+            Parser.throwParserException(rootNode, "Declaration Node : Second token must be an identifier.");
+        }
+
+        // TODO: when that infernal incrementation has been removed, reintroduce the below code:
+
+        // else if (tokens[2].tokenType != TokenType.Assignment && tokens[2].tokenType != TokenType.Semicolon)
+        // {
+        //     Parser.throwParserException(rootNode, "Declaration Node : Third token must be either an assignment symbol or a semicolon.");
+        // }
+
+        // else if (tokens[tokens.length - 1].tokenType != TokenType.Semicolon)
+        // {
+        //     System.out.println(tokens[tokens.length - 1].tokenValue);
+        //     Parser.throwParserException(rootNode, "Declaration Node : Last token must be a semicolon.");
+        // }
+	}
+
+	@Override
     public String toString()
     {
         String output = "";
