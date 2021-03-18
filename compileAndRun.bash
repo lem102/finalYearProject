a5aad89f0cd14658aca0c90492a7df7b5525f132
lem <jacobtophatleeming@gmail.com>
2021-03-18 21:40:10 +0000

    updated bash script

--- compileAndRun.bash
+++ compileAndRun.bash
@@ -4,9 +4,13 @@ if javac -g -d bin -cp bin $(find . -name "*.java")
 then
     if java -cp bin com.jpl.fyp.Main $1 $2
     then
-        nasm -f elf output.asm
-        ld -m elf_i386 output.o -o output
-        ./output
+        if nasm -f elf output.asm
+        then
+            if ld -m elf_i386 output.o -o output
+            then
+                ./output
+            fi
+        fi
     fi
     rm -rf bin/*
 fi
